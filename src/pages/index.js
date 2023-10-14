6be6e731c7a29df3adca0a797769effdad3dcb14
Francisco Garcia <franciscogarcia@Macbook-air-de-Fran.local>
2023-10-14 12:54:40 +0200

    1 cambio

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        modifiado desde masD
+        modificado en deploy
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
